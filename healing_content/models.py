dec3f15e0575e9ba07b005e6e4e978fbc361bfce
soroush130 <soroosh1381amir@gmail.com>
2023-08-08 19:06:39 -0700

    fix:Remove Media and Practice into app healing_content

--- healing_content/models.py
+++ healing_content/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from illness.models import Illness, HealingPeriod
 
 
-class HealingContentBase(models.Model):
+class HealingContent(models.Model):
     TYPE_HEALING_CONTENT = (
         ('MEDIA', 'ویدیو'),
         ('PRACTICE', 'تمرین')
@@ -12,11 +12,6 @@ class HealingContentBase(models.Model):
     healing_period = models.ForeignKey(HealingPeriod, on_delete=models.CASCADE, verbose_name='دوره درمانی ')
     type = models.CharField(max_length=8, choices=TYPE_HEALING_CONTENT)
 
-    class Meta:
-        abstract = True
-
-
-class Media(HealingContentBase):
     file = models.FileField(upload_to='healing_content/media_practice/', verbose_name='فایل ')
     day = models.PositiveIntegerField(default=1, verbose_name='شماره روز', help_text='روز چندم از دوره درمانی')
 
@@ -26,9 +21,9 @@ class Media(HealingContentBase):
 
         total_days_in_healing_period = self.healing_period.duration_of_treatment * 7
         if day > total_days_in_healing_period:
-            raise ValidationError("day more than total days in healing period")
+            raise ValidationError("خارج از بازه زمانی دوره درمان است")
 
-        existing_day = Media.objects.filter(day=day, healing_period=self.healing_period)
+        existing_day = HealingContent.objects.filter(day=day, healing_period=self.healing_period)
         if existing_day.exists():
             raise ValidationError("برای این روز از دوره محتوا وجود دارد")
 
@@ -36,23 +31,3 @@ class Media(HealingContentBase):
         self.full_clean()  # Run full validation before saving
         return super().save(*args, **kwargs)
 
-
-class Practice(HealingContentBase):
-    # TODO: additional fields for this model
-    day = models.PositiveIntegerField(default=1, verbose_name='شماره روز', help_text='روز چندم از دوره درمانی')
-
-    def clean(self):
-        super().clean()
-        day = self.day
-
-        total_days_in_healing_period = self.healing_period.duration_of_treatment * 7
-        if day > total_days_in_healing_period:
-            raise ValidationError("day more than total days in healing period")
-
-        existing_day = Media.objects.filter(day=day, healing_period=self.healing_period)
-        if existing_day.exists():
-            raise ValidationError("برای این روز از دوره محتوا وجود دارد")
-
-    def save(self, *args, **kwargs):
-        self.full_clean()  # Run full validation before saving
-        return super().save(*args, **kwargs)
