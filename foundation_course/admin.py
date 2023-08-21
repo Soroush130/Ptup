a2a99abd38267b4bbca8d5e86a192fc1854cbecd
soroush130 <soroosh1381amir@gmail.com>
2023-08-20 18:53:26 -0700

    feat:Override delete_queryset into QuestionnaireAnswerAdmin model-admin

--- foundation_course/admin.py
+++ foundation_course/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from customers.models import CustomerDiseaseInformation
 from .models import (
     Questionnaire,
     Question,
@@ -22,6 +24,30 @@ admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(QuestionOption)
 
-admin.site.register(QuestionnaireAnswer)
+
+class QuestionnaireAnswerAdmin(admin.ModelAdmin):
+    list_display = ['questionnaire', 'customer', 'answer_time', 'score']
+    model = QuestionnaireAnswer
+    actions = ['delete_selected']
+
+    def delete_queryset(self, request, queryset):
+
+        for obj in queryset:
+            customer = obj.customer
+            disease_information = CustomerDiseaseInformation.objects.filter(
+                customer=customer,
+                is_finished=False
+            ).first()
+
+            if disease_information.is_foundation_course:
+                disease_information.is_foundation_course = False
+                disease_information.save()
+
+                obj.delete()
+            else:
+                obj.delete()
+
+
+admin.site.register(QuestionnaireAnswer, QuestionnaireAnswerAdmin)
 
 admin.site.register(QuestionnaireAnswerDetail)
