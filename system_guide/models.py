2b68b9b848730ec7cd1406c6108e3c80e863c2ea
soroush130 <soroosh1381amir@gmail.com>
2023-09-23 20:08:56 -0700

    Added Guide model into system_guide app

--- system_guide/models.py
+++ system_guide/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Guide(models.Model):
+    title = models.CharField(
+        verbose_name='عنوان ',
+        blank=True,
+        null=True
+    )
+    file = models.FileField(
+        upload_to='system_guide/',
+        verbose_name=' فایل راهنما '
+    )
+
+    class Meta:
+        db_table = 'guide'
